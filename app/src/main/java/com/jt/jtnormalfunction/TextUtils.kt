d97a9f003a1c99f4ebd6a51d411126a0b0ef0edc
万物生 <843454898@qq.com>
2020-09-18 08:40:37 +0800

    test

---
+++ app/src/main/java/com/jt/jtnormalfunction/TextUtils.kt
@@ -0,0 +1,8 @@
+package com.jt.jtnormalfunction
+
+import android.text.TextUtils
+
+
+fun main(args:Array<String>){
+
+}
---
+++ yyh/src/main/java/com/jt/normal/TextUtils.kt
@@ -0,0 +1,9 @@
+package com.jt.normal
+
+import android.util.Log
+
+object TextUtils {
+    fun text(){
+        Log.e("测试","优优团")
+    }
+}
